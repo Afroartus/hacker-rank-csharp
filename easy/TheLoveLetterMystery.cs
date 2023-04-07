12999c8c19322d47414c2715b58986252cdc0ea5
Afroartus <andresro445@gmail.com>
2023-04-07 02:03:24 -0500

    Create: TheLoveLetterMystery.cs TheLoveLetterMysteryTest.cs

---
+++ easy/TheLoveLetterMystery.cs
@@ -0,0 +1,30 @@
+﻿namespace HackerRank.easy;
+
+public class TheLoveLetterMystery{
+
+    public static int ExerciseTheLoveLetterMystery(string s) {
+
+        var arr = s.ToCharArray();
+        var i = 0;
+        var j = arr.Length - 1;
+        var countOperation = 0;
+        while (j >= arr.Length - (arr.Length/2)){
+            var runeUno = arr[i];
+            var runeDos = arr[j];
+            while (runeUno != runeDos){
+                if (runeUno > runeDos){
+                    runeUno --;
+                }
+                else{
+                    runeDos--;
+                }
+                countOperation++;
+            }
+
+            i++;
+            j--;
+        }
+        return countOperation;
+    }
+    
+}
---
+++ easy/TheLoveLetterMysteryTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.easy;
+
+[TestFixture]
+public class TheLoveLetterMysteryTest{
+
+    [Test]
+    public void TestTheLoveLetterMystery() {
+        string[] arrayNumber ={ "abc", "abcba", "abcd", "cba" };
+        int[] expected ={ 2, 0, 4, 2 };
+
+        for (int i = 0; i < arrayNumber.Length; i++){
+            int got = TheLoveLetterMystery.ExerciseTheLoveLetterMystery(arrayNumber[i]);
+            Assert.AreEqual(expected[i], got);
+        }
+    }
+}
