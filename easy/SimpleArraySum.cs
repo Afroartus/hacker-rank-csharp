b011d3b3e3381e2f83ffabdd71c4ecbf6cfac5df
Afroartus <andresro445@gmail.com>
2023-04-07 12:41:22 -0500

    Update: SimpleArraySum.cs SimpleArraySumTest.cs

---
+++ easy/SimpleArraySum.cs
@@ -0,0 +1,7 @@
+﻿namespace HackerRank.easy;
+
+public class SimpleArraySum{
+    public static int ExerciseSimpleArraySum(List<int> ar) {
+        return ar.Sum(a => a);
+    }
+}
---
+++ easy/SimpleArraySumTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.easy;
+
+[TestFixture]
+public class SimpleArraySumTest{
+
+    [Test]
+    public void TestSimpleArraySum() {
+        List<int> arr = new List<int>{1,2,3,4,5};
+        var actual = SimpleArraySum.ExerciseSimpleArraySum(arr);
+        var expected = 15;
+        Assert.AreEqual(expected, actual);
+    }
+}
